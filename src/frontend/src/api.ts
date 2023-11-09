e4f3d9d31394212a60b6c03d02ecb584f6a72e47
Marie Flores <73853856+marieflorescontact@users.noreply.github.com>
2023-11-09 15:35:57 +0000

    Add front api Post route

--- src/frontend/src/api.ts
+++ src/frontend/src/api.ts
@@ -5,8 +5,14 @@ export const apiBaseUrl = import.meta.env.VITE_CHAT_API_URI || '';
 export async function getCompletion(options: ChatRequestOptions, oneShot = false) {
   const apiUrl = options.apiUrl || apiBaseUrl;
 
-  // TODO: complete call to Chat API here
-  // const response =
+  const response = await fetch(`${apiUrl}/chat`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      messages: options.messages,
+    }),
+  });
+  
 
   if (options.stream) {
     return getChunksFromResponse<ChatResponseChunk>(response as Response, options.chunkIntervalMs);
